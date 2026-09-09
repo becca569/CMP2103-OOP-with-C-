9436c5045af8fa171bc4d8ce271cc3a8f21d8279
becca569 <beccalove261@gmail.com>
2026-09-09 09:04:44 +0300

    Taxation asssignment

--- Chapter_03/taxation.cpp
+++ Chapter_03/taxation.cpp
@@ -26,3 +26,52 @@
 // Enter the residence status: 0
 // Enter the taxable income: 4,000,000
 // Tax is 118000.
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int residenceStatus;
+    long double taxableIncome;
+    long double tax = 0.0;
+
+    cout << "(0-Resident, 1-Non-resident)" << endl;
+    cout << "Enter the residence status: ";
+    cin >> residenceStatus;
+
+    cout << "Enter the taxable income: ";
+    cin >> taxableIncome;
+
+    if (residenceStatus == 0) {
+        if (taxableIncome <= 2820000) {
+            tax = 0;
+        } else if (taxableIncome <= 4020000) {
+            tax = (taxableIncome - 2820000) * 0.10;
+        } else if (taxableIncome <= 4920000) {
+            tax = (taxableIncome - 4020000) * 0.20 + 120000;
+        } else if (taxableIncome <= 120000000) {
+            tax = (taxableIncome - 4920000) * 0.30 + 300000;
+        } else {
+            tax = (taxableIncome - 4920000) * 0.30 + 300000 + (taxableIncome - 120000000) * 0.10;
+        }
+    } else if (residenceStatus == 1) {
+        if (taxableIncome <= 4020000) {
+            tax = taxableIncome * 0.10;
+        } else if (taxableIncome <= 4920000) {
+            tax = (taxableIncome - 4020000) * 0.20 + 402000;
+        } else if (taxableIncome <= 120000000) {
+            tax = (taxableIncome - 4920000) * 0.30 + 582000;
+        } else {
+            tax = (taxableIncome - 4920000) * 0.30 + 582000 + (taxableIncome - 120000000) * 0.10;
+        }
+    } else {
+        cout << "Invalid residence status entered." << endl;
+        return 1;
+    }
+
+    cout << "Tax is " << llround(tax) << "." << endl;
+    return 0;
+}
